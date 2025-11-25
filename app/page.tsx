daab2f05b5901571cf58087677026b1eb3ecc06f
Salem Alswil <sa.al9wel@gmail.com>
2025-11-25 15:24:31 +0300

    intilizing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>welcom to home</h1>
+    <div className="w-full h-screen flex flex-col gap-4 justify-center items-center">
+      <h1>welcom to home page</h1>
+      <Link href={"/dashboard"} className="bg-blue-200 p-2 rounded-md">
+        Goto Dashboard
+      </Link>
+    </div>
   );
 }
